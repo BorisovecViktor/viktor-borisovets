d255b85f199c8910ef9912f4565d7c4928ae09e9
Viktor Borisovets <viktorborisovets@gmail.com>
2025-04-22 00:30:31 +0300

    feat: faqs

---
+++ src/assets/icons/plus.tsx
@@ -0,0 +1,22 @@
+import { twMerge } from 'tailwind-merge'
+
+type Props = {
+  className: string
+}
+
+export const PlusIcon = ({ className }: Props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className={twMerge(className)}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M12 4.5v15m7.5-7.5h-15"
+    />
+  </svg>
+)
